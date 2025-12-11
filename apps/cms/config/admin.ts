3b2cfff4df1f9071475bf8d9970e4019ab8aab5a
District Interiors <dev@districtinteriors.com>
2025-12-11 14:53:59 +0500

    Fix CMS startup: More lenient healthcheck, default env vars, longer start_period

--- apps/cms/config/admin.ts
+++ apps/cms/config/admin.ts
@@ -1,13 +1,13 @@
 export default ({ env }) => ({
   auth: {
-    secret: env('ADMIN_JWT_SECRET'),
+    secret: env('ADMIN_JWT_SECRET', 'defaultSecretChangeInProduction'),
   },
   apiToken: {
-    salt: env('API_TOKEN_SALT'),
+    salt: env('API_TOKEN_SALT', 'defaultSaltChangeInProduction'),
   },
   transfer: {
     token: {
-      salt: env('TRANSFER_TOKEN_SALT'),
+      salt: env('TRANSFER_TOKEN_SALT', 'defaultTransferSaltChangeInProduction'),
     },
   },
   url: env('ADMIN_URL', 'http://localhost:1337/admin'),
